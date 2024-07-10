11a03d65251b57a4b7917ca18fb1b8a07bbb0aeb
Zopolis4 <creatorsmithmdt@gmail.com>
2024-07-10 19:38:40 +1000

    Add Crystal/RequireRelative cop

---
+++ lib/rubocop/cop/crystal/require_relative.rb
@@ -0,0 +1,38 @@
+module RuboCop
+  module Cop
+    module Crystal
+      # In ruby, require_relative attempts to load the library relative to the directory of the currently executing file.
+      # Crystal does not have require_relative, but it has the same behavior in the form of require './foo'
+      #
+      # @example
+      #   # bad
+      #   require_relative 'foo'
+      #   require_relative './bar'
+      #   require_relative '../baz'
+      #   require_relative '/qux'
+      #
+      #   # good
+      #   require './foo'
+      #   require './bar'
+      #   require '../baz'
+      #   require '/qux'
+      #
+      class RequireRelative < Base
+        extend AutoCorrector
+        MSG = 'Crystal does not support require_relative.'
+        RESTRICT_ON_SEND = [:require_relative]
+
+        def on_send(node)
+          add_offense(node) do |corrector|
+            if node.first_argument.value.start_with?('.', '/')
+              require_value = node.first_argument.value
+            else
+              require_value = "./#{node.first_argument.value}"
+            end
+            corrector.replace(node, "require '#{require_value}'")
+          end
+        end
+      end
+    end
+  end
+end
--- lib/rubocop/cop/crystal_cops.rb
+++ lib/rubocop/cop/crystal_cops.rb
@@ -1,2 +1,3 @@
 require_relative 'crystal/file_extension'
 require_relative 'crystal/interpolation_in_single_quotes'
+require_relative 'crystal/require_relative'
