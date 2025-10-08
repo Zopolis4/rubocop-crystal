a4a7203bce3c01e13e162cdae4198f294de11136
Zopolis4 <creatorsmithmdt@gmail.com>
2025-10-08 12:08:48 +1100

    Fix Crystal/SafeNavigation on chained method calls

--- spec/rubocop/cop/crystal/safe_navigation_spec.rb
+++ spec/rubocop/cop/crystal/safe_navigation_spec.rb
@@ -13,11 +13,44 @@ RSpec.describe RuboCop::Cop::Crystal::SafeNavigation, :config do
     expect_match_crystal('x = nil')
   end
 
+  it 'registers an offense when using safe navigation at the start of a method chain' do
+    expect_offense(<<~RUBY)
+      x&.size.nil?
+       ^^ Crystal has .try &. instead of &. for safe navigation.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      x.try &.size.nil?
+    RUBY
+
+    expect_match_crystal('x = [1, 5, 3, 1]')
+    expect_match_crystal('x = nil')
+  end
+
+  it 'registers an offense when using safe navigation at the end of a method chain' do
+    expect_offense(<<~RUBY)
+      x.size&.even?
+            ^^ Crystal has .try &. instead of &. for safe navigation.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      x.size.try &.even?
+    RUBY
+
+    expect_match_crystal('x = [3, 6, 2]')
+  end
+
   it 'does not register an offense when not using safe navigation' do
     expect_no_offenses(<<~RUBY)
       x.even?
     RUBY
 
     expect_match_crystal('x = 1')
+  end
+
+  it 'does not register an offense when safe navigation has already been corrected' do
+    expect_no_offenses(<<~RUBY)
+      x.try &.even?
+    RUBY
   end
 end
