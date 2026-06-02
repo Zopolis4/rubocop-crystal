41228f09dcba3cedaf6355b81699e0d10ee7da57
Zopolis4 <creatorsmithmdt@gmail.com>
2026-06-02 13:19:00 +1000

    Fix Crystal/InterpolationInSingleQuotes on unmatched percent literal deliminators inside interpolated strings

--- lib/rubocop/cop/crystal/interpolation_in_single_quotes.rb
+++ lib/rubocop/cop/crystal/interpolation_in_single_quotes.rb
@@ -22,12 +22,26 @@ module RuboCop
         def on_str(node)
           # We're only interested in single-quote deliminated strings.
           return unless node.source.start_with?("'")
+
           # Replace the single quotes deliminating the string with double quotes, and check if the resulting ast is still the same.
           # If it is, the string doesn't have any interpolation to avoid, and we're done here.
-          return if node == parse("\"#{node.source[1..-2]}\"").ast
+          str_content = node.source[1..-2]
+          return if node == parse("\"#{str_content}\"").ast
+
+          left_delim, right_delim = determine_delimiter(str_content)
 
           add_offense(node) do |corrector|
-            corrector.replace(node, "%q(#{node.source[1..-2]})")
+            corrector.replace(node, "%q#{left_delim}#{str_content}#{right_delim}")
+          end
+        end
+
+        private
+
+        # TODO: This does not handle the situation where all possible delimiters are used inside a string.
+        def determine_delimiter(str_content)
+          %w(() [] {} <> ||).each do |delim|
+            # TODO: This is a pretty naive approach, and probably doesn't handle escaped unmatched delimiters properly.
+            return delim[0], delim[1] if str_content.count(delim[0]) == str_content.count(delim[1])
           end
         end
       end
--- spec/rubocop/cop/crystal/interpolation_in_single_quotes_spec.rb
+++ spec/rubocop/cop/crystal/interpolation_in_single_quotes_spec.rb
@@ -3,12 +3,16 @@ RSpec.describe RuboCop::Cop::Crystal::InterpolationInSingleQuotes, :config do
     expect_no_offenses(<<~RUBY)
       "foo"
     RUBY
+
+    expect_match_crystal
   end
 
   it 'does not register an offense when an interpolated string is in double quotes' do
     expect_no_offenses(<<~'RUBY')
       "foo #{bar}"
     RUBY
+
+    expect_match_crystal('bar = "qux"')
   end
 
   it 'does not register an offense when a non-interpolated string is in single quotes' do
@@ -26,5 +30,98 @@ RSpec.describe RuboCop::Cop::Crystal::InterpolationInSingleQuotes, :config do
     expect_correction(<<~'RUBY')
       %q(foo #{bar})
     RUBY
+
+    expect_match_crystal('bar = "qux"')
+  end
+
+  it 'registers an offense when an empty interpolated circle bracket pair is in single quotes' do
+    expect_offense(<<~RUBY)
+      '"()"'
+      ^^^^^^ Crystal does not support the use of single-quote deliminated strings to avoid interpolation.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      %q("()")
+    RUBY
+
+    expect_match_crystal('bar = "qux"')
+  end
+
+  it 'registers an offense when an interpolated circle bracket pair is in single quotes' do
+    expect_offense(<<~RUBY)
+      '"(frob)"'
+      ^^^^^^^^^^ Crystal does not support the use of single-quote deliminated strings to avoid interpolation.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      %q("(frob)")
+    RUBY
+
+    expect_match_crystal('bar = "qux"')
+  end
+
+  it 'registers an offense when an interpolated left circle bracket is in single quotes' do
+    expect_offense(<<~RUBY)
+      '"("'
+      ^^^^^ Crystal does not support the use of single-quote deliminated strings to avoid interpolation.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      %q["("]
+    RUBY
+
+    expect_match_crystal('bar = "qux"')
+  end
+
+  it 'registers an offense when an interpolated right circle bracket is in single quotes' do
+    expect_offense(<<~RUBY)
+      '")"'
+      ^^^^^ Crystal does not support the use of single-quote deliminated strings to avoid interpolation.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      %q[")"]
+    RUBY
+
+    expect_match_crystal('bar = "qux"')
+  end
+
+  it 'registers an offense when an interpolated left square bracket is in single quotes' do
+    expect_offense(<<~RUBY)
+      '"["'
+      ^^^^^ Crystal does not support the use of single-quote deliminated strings to avoid interpolation.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      %q("[")
+    RUBY
+
+    expect_match_crystal('bar = "qux"')
+  end
+
+  it 'registers an offense when an interpolated right square bracket is in single quotes' do
+    expect_offense(<<~RUBY)
+      '"]"'
+      ^^^^^ Crystal does not support the use of single-quote deliminated strings to avoid interpolation.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      %q("]")
+    RUBY
+
+    expect_match_crystal('bar = "qux"')
+  end
+
+  it 'registers an offense when an interpolated pipe is in single quotes' do
+    expect_offense(<<~RUBY)
+      '"|"'
+      ^^^^^ Crystal does not support the use of single-quote deliminated strings to avoid interpolation.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      %q("|")
+    RUBY
+
+    expect_match_crystal('bar = "qux"')
   end
 end
